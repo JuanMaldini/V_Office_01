983e29a1c58c63f28608152c46ef7d4806c9d46b
Juan Maldini <juan.maldini@outlook.com>
2025-09-24 21:59:50 -0300

    del

--- Plugins/OnlineMa4a4b04cf61c7V2/Source/ShareTextures/Private/ShareTexturesLog.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright ShareTextures 2024. All Rights Reserved.
-
-#include "ShareTexturesLog.h"
-
-DEFINE_LOG_CATEGORY(LogShareTextures);
--- Plugins/OnlineMa4a4b04cf61c7V2/Source/ShareTextures/Public/ShareTexturesLog.h
+++
@@ -1,7 +0,0 @@
-// Copyright ShareTextures 2024. All Rights Reserved.
-
-#pragma once
-
-#include <Logging/LogMacros.h>
-
-DECLARE_LOG_CATEGORY_EXTERN(LogShareTextures, Warning, All)
